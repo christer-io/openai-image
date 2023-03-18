2e13ee465040aad29db1a9df1435970d7377a315
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2023-03-18 23:39:50 +0100

    Finnished small fixes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3030;
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
